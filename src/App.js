e55f79272b25e175cb700efb1fe10aab8d10c1bf
Duncan Bain <duncan@bain.io>
2020-01-15 21:19:03 +0000

    Moved state to App component from DrumPad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,61 @@
 import React from 'react';
 import DrumPad from './DrumPad'
 
+const DRUM_PAD = [{
+  keyCode: 81,
+  keyPress: 'Q',
+  id: 'drum-press-1',
+  audioFile: '/audio/drum-press-1.wav'
+},
+{
+  keyCode: 87,
+  keyPress: 'W',
+  id: 'drum-press-2',
+  audioFile: '/audio/drum-press-2.wav'
+},
+{
+  keyCode: 69,
+  keyPress: 'E',
+  id: 'drum-press-3',
+  audioFile: '/audio/drum-press-3.wav'
+},
+{
+  keyCode: 65,
+  keyPress: 'A',
+  id: 'drum-press-4',
+  audioFile: '/audio/drum-press-4.wav'
+},
+{
+  keyCode: 83,
+  keyPress: 'S',
+  id: 'drum-press-5',
+  audioFile: '/audio/drum-press-5.wav'
+},
+{
+  keyCode: 68,
+  keyPress: 'D',
+  id: 'drum-press-6',
+  audioFile: '/audio/drum-press-6.wav'
+},
+{
+  keyCode: 90,
+  keyPress: 'Z',
+  id: 'drum-press-7',
+  audioFile: '/audio/drum-press-7.wav'
+},
+{
+  keyCode: 88,
+  keyPress: 'X',
+  id: 'drum-press-8',
+  audioFile: '/audio/drum-press-8.wav'
+},
+{
+  keyCode: 67,
+  keyPress: 'C',
+  id: 'drum-press-9',
+  audioFile: '/audio/drum-press-9.wav'
+}
+]
 
 class App extends React.Component {
   constructor(props) {
@@ -8,10 +63,10 @@ class App extends React.Component {
     this.state = {
       thisButtonIsActive: '',
       audioIsActive: false,
-      activeDisplay: 'Press a key to play the drums!'
+      activeDisplay: 'Press a key to play the drums!',
+      drumPad: DRUM_PAD
     };
 
-    this.handleClick = this.handleClick.bind(this);
 
   }
 
@@ -43,7 +98,7 @@ class App extends React.Component {
       </div>
       <div className="row justify-content-center text-center">
         <section className="col-6 bg-light">
-        <DrumPad  handleClick={this.handleClick}/>
+        <DrumPad  drumPad={this.state.drumPad}/>
         </section>
       </div>
 
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,65 +1,11 @@
 import React from 'react';
 
-function DrumPad() {
+function DrumPad(props) {
 
-    const DRUM_PAD = [{
-            keyCode: 81,
-            keyPress: 'Q',
-            id: 'drum-press-1',
-            audioFile: '/audio/drum-press-1.wav'
-        },
-        {
-            keyCode: 87,
-            keyPress: 'W',
-            id: 'drum-press-2',
-            audioFile: '/audio/drum-press-2.wav'
-        },
-        {
-            keyCode: 69,
-            keyPress: 'E',
-            id: 'drum-press-3',
-            audioFile: '/audio/drum-press-3.wav'
-        },
-        {
-            keyCode: 65,
-            keyPress: 'A',
-            id: 'drum-press-4',
-            audioFile: '/audio/drum-press-4.wav'
-        },
-        {
-            keyCode: 83,
-            keyPress: 'S',
-            id: 'drum-press-5',
-            audioFile: '/audio/drum-press-5.wav'
-        },
-        {
-            keyCode: 68,
-            keyPress: 'D',
-            id: 'drum-press-6',
-            audioFile: '/audio/drum-press-6.wav'
-        },
-        {
-            keyCode: 90,
-            keyPress: 'Z',
-            id: 'drum-press-7',
-            audioFile: '/audio/drum-press-7.wav'
-        },
-        {
-            keyCode: 88,
-            keyPress: 'X',
-            id: 'drum-press-8',
-            audioFile: '/audio/drum-press-8.wav'
-        },
-        {
-            keyCode: 67,
-            keyPress: 'C',
-            id: 'drum-press-9',
-            audioFile: '/audio/drum-press-9.wav'
-        }
-    ]
+    
 
-    const drumPadItems = DRUM_PAD.map((item) =>
-        <button key={item.id} className="btn btn-primary drum-pad" id={item.id} onClick={this.props.handleClick}>
+    const drumPadItems = props.drumPad.map((item) =>
+        <button key={item.id} className="btn btn-primary drum-pad" id={item.id} onClick={props.handleClick}>
             <audio id={item.id} src={item.audioFile} />
             {item.keyPress}
             </button>
